717b251a9ac0bab0dddec0f0a11ac91935de872d
Jamezfn <jamesmakachia19@gmail.com>
2025-11-24 12:55:43 +0300

    Implemented the get user with ID method

--- api/crud/user.py
+++ api/crud/user.py
@@ -1,10 +1,9 @@
 from sqlalchemy.orm import Session
 
 from api.models import User
-from schemas.user import UserCreate, ShowUser
 
 
-def create_user(request: UserCreate, db: Session):
+def create_user(request: dict, db: Session):
     new_user = User(
         email=request.email,
         role=request.role
@@ -15,6 +14,10 @@ def create_user(request: UserCreate, db: Session):
     db.refresh(new_user)
     return new_user
 
+def get_user_by_id(user_id: int, db: Session):
+    user = db.query(User).filter(User.id == user_id).first()
+    return user
+
 def get_user_by_email(email: str, db: Session):
     user = db.query(User).filter(User.email == email).first()
-    return ShowUser.model_validate(user)
+    return user
--- api/routers/user.py
+++ api/routers/user.py
@@ -3,17 +3,24 @@ from sqlalchemy.orm import Session
 
 from db import get_db
 from api.models.user import User
-from api.crud.user import create_user, get_user_by_email
-from schemas.user import UserCreate
+from api.crud.user import create_user, get_user_by_email, get_user_by_id
+from schemas.user import UserCreate, ShowUser
 
 router = APIRouter(
     prefix="/users",
     tags=["Users"]
 )
 
-@router.post("/", response_model=UserCreate, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=ShowUser, status_code=status.HTTP_201_CREATED)
 def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = get_user_by_email(email=user.email, db=db)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return create_user(request=user, db=db)
+
+@router.get("/{user_id}", response_model=ShowUser)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = get_user_by_id(user_id=user_id, db=db)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
